ac8a32ea8d878c0b5e5849f4e13811146751315b
Vaibhav <vaibhavbhatt9666@gmail.com>
2024-07-02 00:32:53 +0530

    config changed

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import reactRefresh from '@vitejs/plugin-react-refresh';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),reactRefresh()],
+  build: {
+    rollupOptions: {
+      external: ['react-toastify'], // Add react-toastify to external dependencies
+    },
+  },
 })
